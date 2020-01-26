fbc1cd751fdc0330b72e072afecff16fc46ef833
Andrey <56323140+AndreyNazarov@users.noreply.github.com>
2020-01-26 23:24:58 +0100

    Add files via upload

---
+++ java/task6.js
@@ -0,0 +1,23 @@
+let total = 7;
+
+while (true) {
+    let input = prompt('Tap number');
+
+    if (input === null){
+        console.log('canceled by user');
+        break;
+    }
+
+    input = Number(input);
+
+    const notANumber = Number.isNaN(input);
+
+    if (notANumber) {
+        console.log('not true');
+        continue;
+    }
+
+    total += input;
+}
+
+console.log('total amount ${total}');
